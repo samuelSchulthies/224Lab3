56dbec23da9422123f1692a2c8ab728d545a9f3d
samuelSchulthies <samuelschulthies@gmail.com>
2022-09-15 23:11:15 -0600

    Add initial files

--- binary.c
+++ binary.c
@@ -7,6 +7,7 @@ int main()
     unsigned char c = 'A';
     unsigned int j = 8;
     unsigned int bits [j];
+    int x = (int) c;
 
     // You'll also need an array to store the bit string,
     // either chars or ints will work
@@ -16,13 +17,13 @@ int main()
     */
 
     for (int i = 0; i < j; i++){
-        int x = (int) c;
         if (x%2 == 1){
             bits[i] = 1;
         }
         if (x%2 == 0){
             bits[i] = 0;
         }
+        x = x / 2;
     }
 
 
