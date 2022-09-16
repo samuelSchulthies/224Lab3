ad1add22a0f8cfb63fabeadac4209a71a215f5ff
samuelSchulthies <samuelschulthies@gmail.com>
2022-09-15 23:24:10 -0600

    Add initial files

--- binary.c
+++ binary.c
@@ -5,9 +5,21 @@ int main()
 
     // This is our char being converted
     unsigned char c = 'A';
+    unsigned char u = 'p';
+    unsigned char y = 'w';
+    unsigned char z = 'o';
+    unsigned char v = '!';
+
     unsigned int j = 8;
     unsigned int bits [j];
+
     int x = (int) c;
+    int e = (int) u;
+    int r = (int) y;
+    int t = (int) z;
+    int b = (int) v;
+
+    int collection [5] = {x,e,r,t,b};
 
     // You'll also need an array to store the bit string,
     // either chars or ints will work
@@ -16,22 +28,26 @@ int main()
         Add your binary conversion code here
     */
 
-    for (int i = 0; i < j; i++){
-        if (x%2 == 1){
-            bits[i] = 1;
+    for (int h = 0; h < 5; h++){
+        for (int i = 0; i < j; i++){
+            if (collection[h]%2 == 1){
+                bits[i] = 1;
+            }
+            if (collection[h]%2 == 0){
+                bits[i] = 0;
+            }
+            collection[h] = collection[h] / 2;
         }
-        if (x%2 == 0){
-            bits[i] = 0;
-        }
-        x = x / 2;
     }
 
 
     // Print out the resulting binary number
-    for(int i = j - 1; i >= 0; i--){
-        printf("%d", bits[i]);
+    for (int j = 0; j < 5; ++j){
+        for(int i = j - 1; i >= 0; i--){
+            printf("%d", bits[i]);
+        }
+        printf("\n");
     }
-    printf("\n");
 
     return 0;
 }
